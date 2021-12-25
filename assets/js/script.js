8c6bbd36b3bf23ad5a928ee50f3507310e5816ac
Tom Ainsworth <tomainsworth94@gmail.com>
2021-12-25 22:50:32 +0000

    Add question placeholders

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const questions = [
+  {
+    q1: "?",
+    answer1: {
+      a: "a",
+      b: "b",
+      c: "c",
+      d: "d"
+    },
+    correctAnswer: "a"
+  },
+  {
+    q2: "?",
+    answer2: {
+  a: "a",
+  b: "b",
+  c: "c",
+  d: "d"
+  },
+  correctAnswer: "a"
+},
+{
+  q3: "?",
+  answer3: {
+a: "a",
+b: "b",
+c: "c",
+d: "d"
+},
+correctAnswer: "a"
+},
+{
+  q4: "?",
+  answer4: {
+a: "a",
+b: "b",
+c: "c",
+d: "d"
+},
+correctAnswer: "a"
+},
+{
+  q5: "?",
+  answer5: {
+a: "a",
+b: "b",
+c: "c",
+d: "d"
+},
+correctAnswer: "a"
+},
+];
+
+for (question of questions) {
+  console.log(questions);
+} 

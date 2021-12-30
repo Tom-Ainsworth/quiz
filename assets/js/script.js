28c77e89c822e6e9adf1e102fda5583146c86efe
Tom Ainsworth <tomainsworth94@gmail.com>
2021-12-30 19:48:54 +0000

    Change question answer to an array rather than object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,52 @@
-const questions = [
-  {
-    title: "?",
-    option: {
-      1: "a",
-      2: "b",
-      3: "c",
-      4: "d"
-    },
+const questions = [{
+      title: "?",
+      option: [
+      "a",
+      "b",
+      "c",
+      "d"
+      ],
     correctAnswer: "a"
   },
   {
     title: "?",
-    option: {
-      1: "a",
-      2: "b",
-      3: "c",
-      4: "d"
-  },
-  correctAnswer: "a"
-},
-{
-  title: "?",
-  option: {
-    1: "a",
-    2: "b",
-    3: "c",
-    4: "d"
-},
-correctAnswer: "a"
-},
-{
-  title: "?",
-  option: {
-    1: "a",
-    2: "b",
-    3: "c",
-    4: "d"
-},
-correctAnswer: "a"
-},
-{
-  title: "?",
-  option: {
-a: "a",
-b: "b",
-c: "c",
-d: "d"
-},
-correctAnswer: "a"
-},
-];
+    option: [
+      "a",
+      "b",
+      "c",
+      "d"
+      ],
+    correctAnswer: "a"
+  }, {
+    title: "?",
+    option: [
+      "a",
+      "b",
+      "c",
+      "d"
+      ],
+    correctAnswer: "a"
+  }, {
+    title: "?",
+    option: [
+      "a",
+      "b",
+      "c",
+      "d"
+      ],
+    correctAnswer: "a"
+  }, {
+    title: "?",
+    option: [
+      "a",
+      "b",
+      "c",
+      "d"
+      ],
+    correctAnswer: "a"
+  }, ];
 
 for (question of questions) {
   console.log(questions);
-} 
+}
+

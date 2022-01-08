63d59d1a5d88ffa59650bb3c34f9807ee39aa0fe
Tom Ainsworth <tomainsworth94@gmail.com>
2022-01-08 19:06:09 +0000

    Fix: Restructure showNextQuestion() so that it only runs until the last question. Add alert if submit button == null

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,6 @@ const questions = [{
 const currentQuestion = document.getElementById('current-question');
 const answers = document.getElementById('answer-options');
 
-
 // Question & Score counters
 let runningQuestion = 0;
 const answerOptions = document.getElementById('answer-options');
@@ -44,42 +43,49 @@ let slytherinScore = 0;
 let allScores = `${gryffindorScore}, ${hufflepuffScore}, ${ravenclawScore}, ${slytherinScore}`;
 
 function showNextQuestion() {
-  const questionArray = questions[runningQuestion];
-  document.getElementById('home-page').classList.add('hidden');
-  document.getElementById('quiz-page').classList.remove('hidden');
-  document.getElementById('start-again-btn-div').classList.remove('hidden');
-  currentQuestion.innerHTML = questionArray.title;
+  console.log(runningQuestion, lastQuestion);
+  if (runningQuestion < lastQuestion) {
+    const questionArray = questions[runningQuestion];
+    document.getElementById('home-page').classList.add('hidden');
+    document.getElementById('quiz-page').classList.remove('hidden');
+    document.getElementById('start-again-btn-div').classList.remove('hidden');
+    currentQuestion.innerHTML = questionArray.title;
 
-  // Inject template HTML into the DOM
-  answerOptions.innerHTML = `  
-    <input class="answer-option" type="radio" name="answers" id="option-1" required>
-    <label class="answer-option" for="option-1">
-      <span data-hover="${questionArray.optionA}">${questionArray.optionA}</span>
-    </label>
+    // Inject template HTML into the DOM
+    answerOptions.innerHTML = `  
+      <input class="answer-option" type="radio" name="answers" id="option-1" required>
+      <label class="answer-option" for="option-1">
+        <span data-hover="${questionArray.optionA}">${questionArray.optionA}</span>
+      </label>
 
-    <input class="answer-option" type="radio" name="answers" id="option-2">
-    <label class="answer-option" for="option-2">							
-      <span data-hover="${questionArray.optionB}">${questionArray.optionB}</span>
-    </label>
+      <input class="answer-option" type="radio" name="answers" id="option-2">
+      <label class="answer-option" for="option-2">							
+        <span data-hover="${questionArray.optionB}">${questionArray.optionB}</span>
+      </label>
 
-    <input class="answer-option" type="radio" name="answers" id="option-3">
-    <label class="answer-option" for="option-3">
-      <span data-hover="${questionArray.optionC}">${questionArray.optionC}</span>
-    </label>
+      <input class="answer-option" type="radio" name="answers" id="option-3">
+      <label class="answer-option" for="option-3">
+        <span data-hover="${questionArray.optionC}">${questionArray.optionC}</span>
+      </label>
 
-    <input class="answer-option" type="radio" name="answers" id="option-4">
-    <label class="answer-option" for="option-4">
-      <span data-hover="${questionArray.optionD}">${questionArray.optionD}</span>
-    </label>
-    <br>
-    <button type="submit" id="btn-submit" class="quiz-btn">Submit</button>
-    `;
+      <input class="answer-option" type="radio" name="answers" id="option-4">
+      <label class="answer-option" for="option-4">
+        <span data-hover="${questionArray.optionD}">${questionArray.optionD}</span>
+      </label>
+      <br>
+      <button type="submit" id="btn-submit" class="quiz-btn">Submit</button>
+      `;
 
-  let submitBtn = document.getElementById('btn-submit');
-  submitBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    submitAnswer();
-  });
+      let submitBtn = document.getElementById('btn-submit');
+      submitBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+        submitAnswer();
+      });
+  }
+  else {
+    collectResultsAndDisplayHouse();
+  }
+  
 }
 
 function submitAnswer() {
@@ -102,9 +108,9 @@ function checkAnswerResult() {
     runningQuestion++;
   }
   console.log('Running Question is: ' + runningQuestion);
-  if (runningQuestion >= lastQuestion) {
-    collectResultsAndDisplayHouse();
-  }
+  // if (runningQuestion >= lastQuestion) {
+  //   collectResultsAndDisplayHouse();
+  // }
 }
 
 /**
@@ -129,16 +135,18 @@ function collectResultsAndDisplayHouse() {
   let topScore = finalScores[0];
   document.getElementById('btn-submit').classList.add('hidden');
   document.getElementById('quiz-page').classList.add('hidden');
+  document.getElementById('results-screen').classList.remove('hidden');
+
   if (topScore.house == "Gryffindor") {
     console.log("You're in Gryffindor");
   } else if (topScore.house == "Ravenclaw") {
     console.log("You're in Ravenclaw");
   } else if (topScore.house == "Hufflepuff") {
     console.log("You're in Hufflepuff");
-    document.getElementById('hufflepuff-result').classList.remove('hidden');
+    document.getElementById('gryffindor-result').classList.remove('hidden');
   } else if (topScore.house == "Slytherin") {
     console.log("You're in Slytherin");
-  } 
+  }
 }
 /**
  * Resets all scores and returns to the main screen
